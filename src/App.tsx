9394f0fe36c6579cfb1f733e75b11894f676a099
Leandro <leandrolopez131982@gmail.com>
2023-09-08 10:33:11 -0300

    button de mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import {Greetings} from './kan-mantine-components';
+/* import {Greetings} from 'kan-mantine-components'
+ */import { Button } from '@mantine/core'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <Greetings />
+    <Button variant="gradient" gradient={{ from: 'indigo', to: 'cyan' }}>ENTER</Button>
      <h1>Hola </h1>
+     {/* <Greetings /> */}
     </>
-  )
+  );
 }
 
 export default App
