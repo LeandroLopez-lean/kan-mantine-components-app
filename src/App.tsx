ccdd918c281612a7e36b399c42e078a852b42534
Leandro <leandrolopez131982@gmail.com>
2023-09-28 09:46:55 -0300

    div con el array

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,88 @@
 import { Greetings } from 'kan-mantine-components'
-import { Cards } from 'kan-mantine-components'
 import { Button } from '@mantine/core'
 import './App.css'
 
 function App() {
   const cardsData = [
-    { title: 'Nombre de la capa',
-    category: 'categoria',
-    options : [
-      {
-        icon: "faDownload",
-        onClick: () => console.log('download')
-      },
-      {
-        icon: "faExclamationCircle",
-        onClick: () => console.log('info')
-      },
-      {
-        icon: "faBars",
-        onClick: () => console.log('info')
-      },
-      {
-        icon: "faGear",
-        onClick: () => console.log('config')
-      },
-    ]
-  },
-  { title: 'capacidad',
-    category: 'categoria superficie',
-    options : [
-      {
-        icon: "faDownload",
-        onClick: () => console.log('download')
-      },
-     
-      { 
-        icon: "faGear",
-        onClick: () => console.log('config')
-      }
-    ]
-  },
-  { title: 'capa lineal',
-    category: 'bajada'
-  },
-  { title: 'capa',
-    category: 'base'
-  },
-  { title: 'Nombre',
-    category: 'category'
-  }
-  ]; 
+    {
+      title: 'Nombre de la capa',
+      category: 'categoria',
+      options: [
+        {
+          icon: "faDownload",
+          onClick: () => console.log('download')
+        },
+        {
+          icon: "faExclamationCircle",
+          onClick: () => console.log('info')
+        },
+        {
+          icon: "faBars",
+          onClick: () => console.log('info')
+        },
+        {
+          icon: "faGear",
+          onClick: () => console.log('config')
+        },
+      ]
+    },
+    {
+      title: 'capacidad',
+      category: 'categoria superficie',
+      options: [
+        {
+          icon: "faDownload",
+          onClick: () => console.log('download')
+        },
+
+        {
+          icon: "faGear",
+          onClick: () => console.log('config')
+        }
+      ]
+    },
+    {
+      title: 'capa lineal',
+      category: 'bajada'
+    },
+    {
+      title: 'capa',
+      category: 'base'
+    },
+    {
+      title: 'Nombre',
+      category: 'category'
+    }
+  ];
 
   return (
     <>
-      <div className='App'>
-      <Cards data={cardsData}/>
-    </div> 
-      <Button 
-       >ENTER
-      </Button>
+
+      <Button>ENTER</Button> 
+
       <h1>Hola </h1>
+
       <Greetings />
+     
+      <div>
+      {cardsData.map((card, index) => (
+        <div key={index}>
+          <h2>{card.title}</h2>
+          <p>{card.category}</p>
+          {card.options && (
+            <ul>
+              {card.options.map((option, optionIndex) => (
+                <li key={optionIndex}>
+                  <i className={`fa ${option.icon}`} onClick={option.onClick}></i>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      ))}
+    </div>
+      {/* <Cards>{cardsData}</Cards> */}
+
     </>
   );
 }
